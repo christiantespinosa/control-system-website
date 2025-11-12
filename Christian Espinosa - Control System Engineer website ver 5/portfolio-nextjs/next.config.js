709ce8f37597973b2d48366505c7b9329a986d44
christiantespinosa <christian.espinosa.work@gmail.com>
2025-11-13 01:40:58 +0800

    Update next.config.js

--- Christian Espinosa - Control System Engineer website ver 5/portfolio-nextjs/next.config.js
+++ Christian Espinosa - Control System Engineer website ver 5/portfolio-nextjs/next.config.js
@@ -1,10 +1,7 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+module.exports = {
   output: 'export',
   trailingSlash: true,
   images: {
     unoptimized: true
   }
 };
-
-module.exports = nextConfig;
